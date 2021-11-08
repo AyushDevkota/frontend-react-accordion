5a13a44c32691c66a343b0452ca1c91c42f3b631
Ayush Devkota <Ayushd122@gmail.com>
2021-11-08 20:12:04 +0545

    function updated

--- src/Question.js
+++ src/Question.js
@@ -2,22 +2,19 @@ import React, { useState } from "react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
 const Question = ({ question, answer }) => {
-	const [isAnswer, setIsAnswer] = useState(false);
 	const [isActive, setIsActive] = useState(false);
-	const handleAnswers = () => {
-		setIsActive(!isActive);
-		setIsAnswer(!isAnswer);
-	};
 	return (
 		<div className="question">
 			<div
 				className={`question-title ${isActive ? "active" : ""}`}
-				onClick={handleAnswers}
+				onClick={() => {
+					setIsActive(!isActive);
+				}}
 			>
 				{question}
-				<button>{isAnswer ? <FaAngleUp /> : <FaAngleDown />}</button>
+				<button>{isActive ? <FaAngleUp /> : <FaAngleDown />}</button>
 			</div>
-			<div className="answer">{isAnswer ? answer : ""}</div>
+			<div className="answer">{isActive ? answer : ""}</div>
 		</div>
 	);
 };
